c462144684e1ce126091f749e9a0f61658138241
Pascal Laliberté <pascal@pascallaliberte.me>
2019-11-11 12:20:35 -0500

    always show heading links

--- _src/heading-links.js
+++ _src/heading-links.js
@@ -3,4 +3,5 @@ const AnchorJS = require('anchor-js')
 const anchors = new AnchorJS()
 
 anchors.options.placement = 'right'
+anchors.options.visible = 'always'
 anchors.add('.book-content h2, .book-content h3')
